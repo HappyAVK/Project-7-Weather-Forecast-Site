34363739077f5e6e33343a706489f6ea19f40376
Alex <alexlsouthall@yahoo.com>
2023-01-06 19:12:20 +0000

    Added some interactivity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import time
+import plotly.express as px
 
-date = time.strftime("%d-%m-%y")
 
 st.title("Weather Forecast")
 place = st.text_input("Place: ")
@@ -11,5 +10,21 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Forecast"))
 
 
-st.subheader(f"{option} for {date} in {place}")
+st.subheader(f"{option} for DATES in {place}")
+
+
+def heat_up_the_sun(days):
+
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+
+    temperatures = [10, 11, 15]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+
+d, t = heat_up_the_sun(days)
+
+figure = px.line(x=d, y=t, labels={"x": "dates", "y": "temperatures(C)"})
+
+st.plotly_chart(figure)
 
