8fc5cd20cadb68fdf9356cd5ebc51424e05e8635
Alex <alexlsouthall@yahoo.com>
2023-01-10 16:01:12 +0000

    Added images to reflect Weather forecast, added exception error for incorrect location names, adjusted temperature values to be more accurate

--- backend.py
+++ backend.py
@@ -1,25 +1,18 @@
 import requests
 
 
-def get_data(place, forecast_days, forecast_type):
+def get_data(place, forecast_days):
     api_key = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid=d65ec62fc756db4c55b20c412c4c24c1"
 
     r = requests.get(api_key)
     cont = r.json()
-    data = cont["list"]  # #"list" contains dictionaries for each day's temperature
+    data = cont["list"]  # #list contains dictionaries for each day's temperature
     # There is a temperature reading every 3 hours, so to get data for all the requested days we times the amount by 3
     value = 8 * forecast_days
     new_data = data[:value]
-
-    if forecast_type == "Temperature":
-
-        new_data = [i["main"]["temp"] for i in new_data]
-    elif forecast_type == "Forecast":
-
-        new_data = [i["weather"][0]['main'] for i in new_data]
 
     return new_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="tokyo", forecast_days=4, forecast_type="Temperature"))
+    print(get_data(place="tokyo", forecast_days=4,))
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from backend import get_data
 st.title("Weather Forecast")
 place = st.text_input("Place: ")
 
+sky_dict = {"Clear": "images/clear.png", "Clouds" : "images/clouds.png", "Rain" : "images/rain.png", "Snow" : "images/snow.png"}
 
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Look at the forecast five days ahead from the current date")
@@ -15,18 +16,27 @@ option = st.selectbox("Select data to view", ("Temperature", "Forecast"))
 st.subheader(f"{option} for DATES in {place}")
 
 
-def heat_up_the_sun(days):
-
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+if place:
+    try:
+        new_data = get_data(place, days)
+        Error_msg = ""
 
 
-d, t = heat_up_the_sun(days)
 
-figure = px.line(x=d, y=t, labels={"x": "dates", "y": "temperatures(C)"})
 
-st.plotly_chart(figure)
+        if option == "Temperature":
+            Temp = [i["main"]["temp"] for i in new_data]
+            Temp = [int(x/10) for x in Temp]
+            Dates = [i["dt_txt"] for i in new_data]
+            figure = px.line(x=Dates, y=Temp, labels={"x": "dates", "y": "temperatures(C)"})
+
+            st.plotly_chart(figure)
+        elif option == "Forecast":
+            Sky_cond = [i["weather"][0]['main'] for i in new_data]
+
+            current_sky = [sky_dict[condition] for condition in Sky_cond]
+            st.image(current_sky, width=200)
+
+    except LookupError:
+        Error_msg = st.info("Enter a real world city")
 
